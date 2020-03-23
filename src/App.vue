a5252c840cabfa48830fae3abe2ab589978c1c59
Bruno Bristot <brunobristot@yahoo.com.br>
2020-03-23 10:43:32 -0300

    Commit inicial sigacarros front

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Anuncio/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import Anuncio from '@/components/Anuncio.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Anuncio
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="@/assets/main.css">
 </style>
